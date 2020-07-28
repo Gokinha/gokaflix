0dc0df2f59ea747828ccb127b1c3608aa283a947
Gokinha <43858731+Gokinha@users.noreply.github.com>
2020-07-28 20:46:20 -0300

    Segundo dia da aula Imersão React

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Logo from '../../assets/img/Logo.png';
-import './Menu.css';
-import Button from './Button';
-
+import React from "react";
+import {Link} from 'react-router-dom';
+import Logo from "../../assets/img/Logo.png";
+import "./Menu.css";
+import Button from "./Button";
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <a href="/home">
-              <img className="Logo" src={Logo} alt="GokaFlix logo"/>
-            </a>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="GokaFlix logo" />
+      </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
-                Novo Vídeo
-            </Button>
-        </nav>
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/Video">
+        Novo Vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
