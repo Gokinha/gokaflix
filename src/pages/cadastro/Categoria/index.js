3c2437c6b9933ffcaedb1f03656ba2a5cfd3ba53
Gokinha <43858731+Gokinha@users.noreply.github.com>
2020-07-28 21:03:50 -0300

    Correção Carousel

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,8 +7,12 @@ function CadastroCategoria(){
     <PageDefault>
         <h1>Cadastro de Categoria</h1>
         
-        <Link to="/cadastro/categoria">
-          Cadastrar Categoria
+        <form>
+          <label>Nome da Categoria</label>
+          <inpu></inpu>
+        </form>
+        <Link to="/">
+          Ir para Home
         </Link>
     </PageDefault>
     )
