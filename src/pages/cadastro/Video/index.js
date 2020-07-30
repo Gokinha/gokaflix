4294ee4a57d34e63e0402d5c2199cb26156e94be
Gokinha <43858731+Gokinha@users.noreply.github.com>
2020-07-29 23:57:38 -0300

    Terceiro dia Imersão React

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PageDefault from "../../../components/PageDefault";
+import { Link } from "react-router-dom";
 
-function CadastroVideo(){
-    return (
+function CadastroVideo() {
+  return (
     <PageDefault>
-        <h1>Cadastro de Vídeo</h1>
-        
-        <Link to="/cadastro/categoria">
-          Cadastrar Categoria
-        </Link>
+      <h1>Cadastro de Vídeo</h1>
+      <form>
+        <label>Nome da Video</label>
+        <input type="text"></input>
+        <button type="submit">Cadastrar</button>
+      </form>
+      <Link to="/cadastro/categoria">Cadastrar Categoria</Link>
     </PageDefault>
-    )
-  }
+  );
+}
 
-  export default CadastroVideo;
+export default CadastroVideo;
